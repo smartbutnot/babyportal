1b696ebbbffd9b9352776038b33b4215ae4bf36d
smartbutnot <103124527+smartbutnot@users.noreply.github.com>
2024-01-07 18:19:31 +0000

    Create secrets.py

---
+++ matrixportal/secrets.py
@@ -0,0 +1,12 @@
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk committing that info or sharing it
+
+secrets = {
+    'ssid' : 'WIFI NETWORK',
+    'password' : 'WIFI PASSWORD',
+    'timezone' : "Europe/London", # http://worldtimeapi.org/timezones
+    'broker' : 'MQTT IP',
+    'user' : 'MQTT USER',
+    'pass' : 'MQTT PASSWORD',
+    'port' : 1883,
+    }
